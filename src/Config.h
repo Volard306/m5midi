847a6d6789474600cecdd2cf7eaf277253cd4974
Volard306 <d351m8@gmail.com>
2026-07-04 21:18:00 -0600

    Delete src directory

--- src/Config.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-constexpr int NUM_ENCODERS = 16;
--- src/EventQueue.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Events.h"
-
-class EventQueue {
-public:
-  void push(const Event& e);
-  bool pop(Event& e);
-  void process();
-};
